ba7b6c758d765d9298e37698dc95e966fe558507
Jesse Hill <jsnahill@gmail.com>
2017-02-17 16:07:05 -0800

    Draw svg enemy elements

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,20 @@
 // start slingin' some d3 here.
+
+var svgContainer = d3.select(".board").append("svg")
+  .attr("width",500)
+  .attr("height",500);
+
+var enemyGroup = svgContainer.append("g");
+
+var enemies = enemyGroup.selectAll("circle")
+  .data([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+  .enter()
+  .append("circle");
+
+var enemyAttributes = enemies
+  .attr("cx", function (d, i) { return 40; })
+  .attr("cy", function (d, i) { return i * 30 + 20; })
+  .attr("r", function (d) { return 20; })
+  .style("fill", function (d) { return "red"; });
+
+
